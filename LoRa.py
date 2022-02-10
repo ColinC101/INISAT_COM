331acf30b72a85e276f104b91d7641622db1235b
BenPer <benoit.perroux@gmail.com>
2022-02-10 13:46:39 +0100

    General LoRa done. TODO: Add data in sendReadings()

--- LoRa.py
+++ LoRa.py
@@ -11,6 +11,11 @@ For this implementation, we are focusing on LoPy to Lopy communication,
 in simple LoRa mode (see https://docs.pycom.io/tutorials/networks/lora/module-module/)
 """
 
+
+# Status of th LoRa link. (0 = disabled / 1 = enabled)
+__loraStatus__ = True
+
+
 loraMode = LoRa.LORA
 loraRegion = LoRa.EU868  # Europe
 
@@ -36,6 +41,10 @@ loraDeviceClass = 0
 lora = LoRa(mode=loraMode, region=loraRegion)
 
 def initLoRa():
+    """
+    Initialisation of the LoRa transmission.
+    Parameters (mode, region, ...) can be modified in config.py file
+    """
     print("Initializing LoRa ...")
     lora.init(mode=loraMode, region=loraRegion,
             tx_power=loraTxPower, bandwidth=loraBandwidth,
@@ -46,8 +55,58 @@ def initLoRa():
     
     print("LoRa initialized")
 
+######################################################
+#################  CONNEXION STATUS  #################
+######################################################
 
-def loopSend():
+def getLoraStatus():
+    """
+    Gets the status of the Lora link
+    @return True  if the link is On
+            False if the link is Off
+    """
+    return __loraStatus__
+
+def enableLora():
+    """
+    Enables the LoRa link.
+    Communications will then be sent by the LoRa Antenna
+    """
+    __loraStatus__ = True
+
+def disableLora():
+    """
+    Disnables the LoRa link.
+    Communications will not be sent by the LoRa Antenna
+    """
+    __loraStatus__ = False
+
+
+def sendReadings():
+    """
+    Sends the collected data via the LoRa link.
+    """
+    # Creating communication Socket
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    s.setblocking(False)
+    # TODO: Ajouter les envois des données
+        # (quand les données seront ajoutées)
+    s.close()
+
+
+
+
+
+######################################################
+##############  TESTS DE FONCTIONNEMENT ##############
+######################################################
+
+
+def testLoopSend():
+    """
+    A simple loop sending LoRa Ping messages every second.
+    To do a complete test, please use testLoopReceive() on another node.
+    """
     # Creating communication Socket
     s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
     s.setblocking(False)
@@ -60,7 +119,11 @@ def loopSend():
         time.sleep(1)
     s.close
 
-def loopReceive():
+def testLoopReceive():
+    """
+    A simple loop waiting for LoRa Ping messages, and responding when received.
+    To do a complete test, please use testLoopSend() on another node.
+    """
     # Creating communication Socket
     s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
     s.setblocking(False)
