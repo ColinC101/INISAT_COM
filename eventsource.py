03264c139509842d3296da00cad3c83d1cec1373
Farges Jean-loup <jeanloup.farges@etu.enseeiht.fr>
2022-02-13 11:42:14 +0100

    EventSource changed : single socket

--- eventsource.py
+++ eventsource.py
@@ -9,21 +9,26 @@ class EventSource:
         @arg name(str) : the name of the event
         """
         self.name = name
-        self.socketClientList = []
+        self.lnkSocket = None
 
     def bind(self,socketClient):
         """
-        Bind this event source to the given socket
+        Bind this event source to the given socket. Note : if a socket was already bound to this
+        EventSource, it will be closed.
         @arg socketClient(--) : the name of the socket client to bind to this
         event
         """
-        self.socketClientList.append(socketClient)
+        if(self.lnkSocket != None):
+            self.lnkSocket.close()
+
+        self.lnkSocket = socketClient
+
         responseHeader = b"HTTP/1.1 200 OK\r\nCache-Control: no-cache\r\nContent-Type: text/event-stream\r\n\r\n"
         socketClient.send(responseHeader)
     
     def send(self,func_name,data,id):
         """
-        Send an event to all the socket clients that are bound to this event source
+        Send an event to the socket client that is bound to this event source
         @arg func_name(str) : the name of the function to call in the browser
         @arg data(str) : the data associated to the event
         @arg id(int) : a unique ID to identify this event occurrence
@@ -32,27 +37,9 @@ class EventSource:
         messageBody += "event: "+func_name+"\n"
         messageBody += "data: "+data+"\n\n"
         
-        # List to store the index of sockets that hasve been closed and has 
-        # to be cleared
-        removeSocketIdx = []
-
-        idx = 0
-        for socketClient in self.socketClientList:
-            try:
-                socketClient.send(bytes(messageBody, 'utf-8'))
-            except OSError as err:
-                socketClient.close()
-                removeSocketIdx.append(idx)
-                print("Err no:"+str(err.errno))
-            idx += 1
-
-        # Reverse the list in order to remove properly the items
-        # (starting from the high index)
-        removeSocketIdx.reverse()
-
-        # Clear closed sockets
-        for socketIdx in removeSocketIdx:
-            del self.socketClientList[socketIdx]
-
-
-    
+        try:
+            self.lnkSocket.send(bytes(messageBody, 'utf-8'))
+        except OSError as err:
+            # Handle connection reset
+            self.lnkSocket.close()
+            self.lnkSocket = None
