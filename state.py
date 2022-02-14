f12c4a1c9e0f23bde4740b3f5e0dad523c0a1e42
Jean-Loup <jafarges@gmail.com>
2022-02-14 13:06:43 +0100

    State variables

---
+++ state.py
@@ -0,0 +1,26 @@
+def init():
+    """
+    Init state variables
+    """
+    global consoleConfig,autoTesting,consoleInterval,modeGnss,camStatus,udpCom,testMag
+
+    # Console configuration
+    consoleConfig = "00000000000000"
+
+    # Auto test status
+    autoTesting = 0
+
+    # Console updating interval (ms)
+    consoleInterval = 5000
+
+    # Enable / Disable GNSS
+    modeGnss = 0
+
+    # Enable / Disable camera
+    camStatus = 0
+
+    # Enable / Disable UDP com
+    udpCom = False
+
+    # Active magneto-coupler test
+    testMag = 0
