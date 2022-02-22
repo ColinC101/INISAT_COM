df99cb3c6bba2e203951e8b52dc49d1873c2eeb3
BenPer <benoit.perroux@gmail.com>
2022-02-22 14:57:25 +0100

    renamed keys without changing the aliases (for easier manipulation in web interface)

--- aliases.py
+++ aliases.py
@@ -45,102 +45,102 @@ UART_COMMAND_END = 'Z'
 #######################################
 
 # Autotests
-JSON_AUTOTEST_WIFI_MODE = "var1"
-JSON_AUTOTEST_WIFI_POWER = "var2"
-JSON_AUTOTEST_WIFI_IP = "var3"
-JSON_AUTOTEST_PING_CAMERA = "var4"
-JSON_AUTOTEST_LORA_POWER = "var5"
-JSON_AUTOTEST_LORA_SPREADINGFACTOR = "var6"
-JSON_AUTOTEST_LORA_BANDWIDTH = "var7"
-JSON_AUTOTEST_LORA_FREQUENCY = "var8"
-JSON_AUTOTEST_LORA_CODINGRATE = "var9"
-JSON_AUTOTEST_LORA_PREAMBLE = "var10"
-JSON_AUTOTEST_VOID1 = "var11"
-JSON_AUTOTEST_VOID2 = "var12"
-JSON_AUTOTEST_VOID3 = "var13"
-JSON_AUTOTEST_RAW = "var14"
+JSON_AUTOTEST_WIFI_MODE = "autotest_wifi_mode"
+JSON_AUTOTEST_WIFI_POWER = "autotest_wifi_power"
+JSON_AUTOTEST_WIFI_IP = "autotest_wifi_ip"
+JSON_AUTOTEST_PING_CAMERA = "autotest_ping_camera"
+JSON_AUTOTEST_LORA_POWER = "autotest_lora_power"
+JSON_AUTOTEST_LORA_SPREADINGFACTOR = "autotest_lora_spedingfactor"
+JSON_AUTOTEST_LORA_BANDWIDTH = "autotest_lora_bandwidth"
+JSON_AUTOTEST_LORA_FREQUENCY = "autotest_lora_frequency"
+JSON_AUTOTEST_LORA_CODINGRATE = "autotest_lora_codingrate"
+JSON_AUTOTEST_LORA_PREAMBLE = "autotest_lora_preamble"
+JSON_AUTOTEST_VOID1 = "autotest_void1"
+JSON_AUTOTEST_VOID2 = "autotest_void2"
+JSON_AUTOTEST_VOID3 = "autotest_void3"
+JSON_AUTOTEST_RAW = "autotest_raw"
 
 
 # EPS
-JSON_EPS_VBAT = "var1"
-JSON_EPS_VIN = "var2"
-JSON_EPS_VOUT = "var3"
-JSON_EPS_ICHARGE = "var4"
-JSON_EPS_IIN = "var5"
-JSON_EPS_TBAT = "var6"
-JSON_EPS_CHARGE_STATUS = "var7"
+JSON_EPS_VBAT = "eps_vbat"
+JSON_EPS_VIN = "eps_vin"
+JSON_EPS_VOUT = "eps_vout"
+JSON_EPS_ICHARGE = "eps_icharge"
+JSON_EPS_IIN = "eps_iin"
+JSON_EPS_TBAT = "eps_tbat"
+JSON_EPS_CHARGE_STATUS = "eps_charge_status"
 
 # Temperature
-JSON_TEMPERATURE = "var8"
+JSON_TEMPERATURE = "temperature"
 
 # Altitude
-JSON_ALTITUDE = "var9"
+JSON_ALTITUDE = "altitude"
 
 # Pression
-JSON_PRESSION = "var10"
+JSON_PRESSION = "pression"
 
 # Euler
-JSON_EULER_ROLL = "var11"
-JSON_EULER_PITCH = "var12"
-JSON_EULER_YAW = "var13"
+JSON_EULER_ROLL = "euler_roll"
+JSON_EULER_PITCH = "euler_pitch"
+JSON_EULER_YAW = "euler_yaw"
 
 # Quaternion
-JSON_QUATERNION_W = "var14"
-JSON_QUATERNION_X = "var15"
-JSON_QUATERNION_Y = "var16"
-JSON_QUATERNION_Z = "var17"
+JSON_QUATERNION_W = "quaternion_w"
+JSON_QUATERNION_X = "quaternion_x"
+JSON_QUATERNION_Y = "quaternion_y"
+JSON_QUATERNION_Z = "quaternion_z"
 
 # Angular Speed
-JSON_ANGULARSPEED_X = "var18"
-JSON_ANGULARSPEED_Y = "var19"
-JSON_ANGULARSPEED_Z = "var20"
+JSON_ANGULARSPEED_X = "angularspeed_x"
+JSON_ANGULARSPEED_Y = "angularspeed_y"
+JSON_ANGULARSPEED_Z = "angularspeed_z"
 
 # Acceleration
-JSON_ACCELERATION_X = "var21"
-JSON_ACCELERATION_Y = "var22"
-JSON_ACCELERATION_Z = "var23"
+JSON_ACCELERATION_X = "acceleration_x"
+JSON_ACCELERATION_Y = "acceleration_y"
+JSON_ACCELERATION_Z = "acceleration_z"
 
 # Magnetic Field
-JSON_MAGNETICFIELD_X = "var24"
-JSON_MAGNETICFIELD_Y = "var25"
-JSON_MAGNETICFIELD_Z = "var26"
+JSON_MAGNETICFIELD_X = "magneticfield_x"
+JSON_MAGNETICFIELD_Y = "magneticfield_y"
+JSON_MAGNETICFIELD_Z = "magneticfield_z"
 
 # Linear acceleration
-JSON_LINEARACCELERATION_X = "var27"
-JSON_LINEARACCELERATION_Y = "var28"
-JSON_LINEARACCELERATION_Z = "var29"
+JSON_LINEARACCELERATION_X = "linearacceleration_x"
+JSON_LINEARACCELERATION_Y = "linearacceleration_y"
+JSON_LINEARACCELERATION_Z = "linearacceleration_z"
 
 # Gravity Vector
-JSON_GRAVITY_X = "var30"
-JSON_GRAVITY_Y = "var31"
-JSON_GRAVITY_Z = "var32"
+JSON_GRAVITY_X = "gravity_x"
+JSON_GRAVITY_Y = "ravity_y"
+JSON_GRAVITY_Z = "gravity_z"
 
 # Luminance
-JSON_LUMINANCE_X = "var33"
-JSON_LUMINANCE_NEGX = "var34"
-JSON_LUMINANCE_Y = "var35"
-JSON_LUMINANCE_NEGY = "var36"
-JSON_LUMINANCE_Z = "var37"
+JSON_LUMINANCE_X = "luminance_x"
+JSON_LUMINANCE_NEGX = "luminance_negx"
+JSON_LUMINANCE_Y = "luminance_y"
+JSON_LUMINANCE_NEGY = "luminance_negy"
+JSON_LUMINANCE_Z = "luminance_z"
 
 # GNSS
-JSON_GNSS_UTC = "var38"
-JSON_GNSS_LATITUDE = "var39"
-JSON_GNSS_LONGITUDE = "var40"
-JSON_GNSS_NBSAT = "var41"
+JSON_GNSS_UTC = "gnss_utc"
+JSON_GNSS_LATITUDE = "gnss_latitude"
+JSON_GNSS_LONGITUDE = "gnss_longitude"
+JSON_GNSS_NBSAT = "gnss_nbsat"
 
 # NMEA
-JSON_NMEA_1 = "var42"
-JSON_NMEA_2 = "var43"
-JSON_NMEA_3 = "var44"
-JSON_NMEA_4 = "var45"
-JSON_NMEA_5 = "var46"
-JSON_NMEA_6 = "var47"
-JSON_NMEA_7 = "var48"
-JSON_NMEA_8 = "var49"
-JSON_NMEA_9 = "var50"
+JSON_NMEA_1 = "nmea_1"
+JSON_NMEA_2 = "nmea_2"
+JSON_NMEA_3 = "nmea_3"
+JSON_NMEA_4 = "nmea_4"
+JSON_NMEA_5 = "nmea_5"
+JSON_NMEA_6 = "nmea_6"
+JSON_NMEA_7 = "nmea_7"
+JSON_NMEA_8 = "nmea_8"
+JSON_NMEA_9 = "nmea_9"
 
 # Config
-JSON_CONS_CONFIG = "var51"
+JSON_CONS_CONFIG = "cons_config"
 
 
 
